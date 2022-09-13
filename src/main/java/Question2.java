65b07b5fe9c52f3a28a19903b0443364f869649d
JoeDirrtyy <joeymarkowski96@gmail.com>
2022-09-13 15:23:00 -0400

    Question 3 done, question 2 need help on. question 3 did not attempt (yet)

--- src/main/java/Question2.java
+++ src/main/java/Question2.java
@@ -1,15 +1,19 @@
 public class Question2 {
 //Referenced from Question 4 homework
+ // Referenced from https://stackoverflow.com/questions/40222495/using-all-elements-in-an-array-except-for-one
 
     public static char getLetterY(char[] alphabet){
-        for (char Y = 0; Y < alphabet.length; Y++){
-
+       boolean $ = false;
+        for (char y = 1; y < alphabet.length; y++){
+        if (alphabet[y] == 0){
+            $ = true;
         }
-        return alphabet[24];
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
         char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','$','Z'};
-        System.out.println(getLetterY(alphabet));
+        System.out.println(alphabet);
     }
 }
---
+++ src/main/java/Question3.java
@@ -0,0 +1,15 @@
+public class Question3 {
+    // referenced from https://www.javatpoint.com/how-to-print-array-in-java
+    // referenced from https://stackoverflow.com/questions/30734252/how-do-you-print-certain-number-in-an-array
+
+    public static void main(String[] args) {
+        int[] array = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,0};
+        for (int x = 0; x < 11; x++) {
+            if (array[x] % 2 != 9) {
+
+
+                System.out.println(array[x]);
+            }
+        }
+    }
+}
