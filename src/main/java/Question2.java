6aa822562d18d9a7acd5ade4c522f19d01fb6bd7
JoeDirrtyy <joeymarkowski96@gmail.com>
2022-09-13 14:01:10 -0400

    only question 2 and did not finish

---
+++ src/main/java/Question2.java
@@ -0,0 +1,15 @@
+public class Question2 {
+//Referenced from Question 4 homework
+
+    public static char getLetterY(char[] alphabet){
+        for (char Y = 0; Y < alphabet.length; Y++){
+
+        }
+        return alphabet[24];
+    }
+
+    public static void main(String[] args) {
+        char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','$','Z'};
+        System.out.println(getLetterY(alphabet));
+    }
+}
